bf15ae43214331d43bfe094e21c2d8644316da1a
정재현 <wolgus104@naver.com>
2025-03-13 22:09:47 +0900

    express-async-error deleteStockFromPortfolio 에도 적용

--- src/controllers/portfolioController.ts
+++ src/controllers/portfolioController.ts
@@ -77,21 +77,11 @@ export const deleteStockFromPortfolio = async (
 ): Promise<void> => {
   const { symbol } = req.params
 
-  try {
-    const result = await deleteStockFromPortfolioService(symbol)
+  const result = await deleteStockFromPortfolioService(symbol)
 
-    if (result.message.includes("존재하지 않습니다")) {
-      res.status(404).json(result)
-      return
-    }
-
-    res.status(200).json(result)
-    return
-  } catch (error) {
-    console.error("Error deleting stock from portfolio:", error)
-    res
-      .status(500)
-      .json({ message: "포트폴리오에서 주식을 삭제하는데 실패했습니다." })
-    return
+  if (result.message.includes("존재하지 않습니다")) {
+    throw createError(404, "존재하지 않는 주식은 삭제할 수 없습니다.")
   }
+
+  res.status(200).json(result)
 }
