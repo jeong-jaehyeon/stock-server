c38be590ba17666610b6bf400a0b6fec24cc9ae4
정재현 <wolgus104@naver.com>
2025-04-07 23:07:54 +0900

    user model 생성 및 db 동기화

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,13 +1,14 @@
 import Portfolio from "./Portfolio"
 import TradeHistory from "./TradeHistory"
+import User from "./User"
 // 다른 모델들도 여기서 임포트합니다
 // import AnotherModel from './AnotherModel';
 
 const models = {
   Portfolio,
   TradeHistory,
+  User,
   // 다른 모델들을 여기에 추가
-  // AnotherModel,
 }
 
 // 관계 설정
